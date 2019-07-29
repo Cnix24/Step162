aceab3c24c5c36e75a41fbd68938cdba13e8697e
Cnix24 <calvinattl24@gmail.com>
2019-07-29 13:55:49 -0600

    Initial commit

---
+++ Step162ConstVarConstructors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Step162ConstVarConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string unchangingThing = "constant";
+            var undeterminedDataType = "might be a number or something";
+
+        }
+    }
+}
---
+++ Step162ConstVarConstructors/twoconstructors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step162ConstVarConstructors
+{
+    class twoconstructors
+    {
+        public twoconstructors(int value) : this(value, "no description")
+        {
+        }
+        public twoconstructors(int value, string description)
+        {
+
+        }
+    }
+}
